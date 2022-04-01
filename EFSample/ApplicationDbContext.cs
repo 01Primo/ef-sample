f36332ac5269d689daa995614a1c4f81e67bee92
Alberto Viezzi <aviezzi@outlook.com>
2022-04-01 09:23:50 +0200

    Initial commit

---
+++ EFSample/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿namespace EFSample;
+
+using System.Data.Common;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<WeatherForecast> WeatherForecast { get; set; } = null!;
+    
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+}
